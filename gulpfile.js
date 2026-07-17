20d57727be9f879b4c9ea91207d2e5f9b0fb5b6d
xycu <smonindemid@gmail.com>
2026-07-17 13:40:41 +0500

    fix: Prevent images from bloating when uploading to the final dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,26 @@ const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer').default;
 const clean = require('gulp-clean');
+const webp = require('gulp-webp').default;
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+
+function convertWebp() {
+    return src('app/images/src/**/*.{png,jpg,jpeg}', { encoding: false })
+        .pipe(newer({ dest: 'app/images/dist', ext: '.webp' }))
+        .pipe(webp())
+        .pipe(dest('app/images/dist'));
+}
+
+function optimizeImages() {
+    return src('app/images/src/**/*.{png,jpg,jpeg,svg}', { encoding: false })
+        .pipe(newer('app/images/dist'))
+        .pipe(imagemin())
+        .pipe(dest('app/images/dist'));
+}
+
+const images = series(convertWebp, optimizeImages);
+
 
 
 function scripts() {
@@ -28,37 +48,37 @@ function styles() {
 }
 
 function watching() {
-    watch(['app/scss/style.scss'], styles)
-    watch(['app/js/main.js'], scripts)
-    watch(['app/*.html']).on('change', browserSync.reload);
-}
-
-function browsersync() {
     browserSync.init({
         server: {
             baseDir: "app/"
         }
     });
+    watch(['app/scss/style.scss'], styles)
+    watch(['app/images/src'], images)
+    watch(['app/js/main.js'], scripts)
+    watch(['app/*.html']).on('change', browserSync.reload);
 }
 
+
 function cleanDist() {
-    return src('dist')
-    .pipe(clean())
+    return src('dist', { allowEmpty: true })
+        .pipe(clean());
 }
 
 function building() {
-    return src([
-        'app/css/style.min.css',
-        'app/js/main.min.js',
-        'app/**/*.html'
-    ], {base: 'app'})
-    .pipe(dest('dist'))
+  return src([
+    'app/css/style.min.css',
+    'app/images/dist/**/*', 
+    'app/js/main.min.js',
+    'app/**/*.html'
+  ], { base: 'app', encoding: false })
+  .pipe(dest('dist'));
 }
 
 exports.styles = styles;
+exports.images = images;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.browsersync = browsersync;
 
-exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.build = series(cleanDist, images, building);
+exports.default = parallel(styles, scripts, watching);
